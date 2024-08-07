073b270334fc4a93ba0d48ee2dddce42eb828669
Matheus Mesquita <matheusmeskitah@gmail.com>
2024-08-07 13:02:57 -0300

    fixed Flows

--- composeApp/src/androidMain/kotlin/core/domain/util/CommonFlow.android.kt
+++ composeApp/src/androidMain/kotlin/core/domain/util/CommonFlow.android.kt
@@ -2,4 +2,4 @@ package core.domain.util
 
 import kotlinx.coroutines.flow.Flow
 
-actual class CommonFlow<T> actual constructor(private val flow: Flow<T>) : Flow<T>
+actual class CommonFlow<T> actual constructor(private val flow: Flow<T>) : Flow<T> by flow
