affbf9388f8f5c9b69728a2e04c927fe398fcd9c
Matheus Mesquita <matheusmeskitah@gmail.com>
2024-09-11 17:30:43 -0300

    Added Di to iOS compose tree

--- composeApp/src/iosMain/kotlin/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/MainViewController.kt
@@ -1,5 +1,9 @@
 import androidx.compose.ui.window.ComposeUIViewController
+import org.kodein.di.compose.withDI
+import translate.di.mainModule
 
 fun MainViewController() = ComposeUIViewController {
-    App()
+    withDI(mainModule) {
+        App()
+    }
 }
