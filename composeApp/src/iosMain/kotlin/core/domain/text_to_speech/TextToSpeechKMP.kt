483eb2127f05e728cae6d0e71e39cc9cf530bb25
Matheus Mesquita <matheusmeskitah@gmail.com>
2024-09-12 09:10:24 -0300

    Added TextToSpeech for iOS

--- composeApp/src/iosMain/kotlin/core/domain/text_to_speech/TextToSpeechKMP.kt
+++ composeApp/src/iosMain/kotlin/core/domain/text_to_speech/TextToSpeechKMP.kt
@@ -1,12 +1,22 @@
 package core.domain.text_to_speech
 
 import core.domain.language.Language
+import platform.AVFAudio.AVSpeechSynthesisVoice.Companion.voiceWithLanguage
+import platform.AVFAudio.AVSpeechSynthesizer
+import platform.AVFAudio.AVSpeechUtterance
 
 actual class TextToSpeechKMP {
+
+    private val synthesizer = AVSpeechSynthesizer()
+    private val utterance = AVSpeechUtterance()
+
     actual fun language(language: Language) {
+        utterance.voice = voiceWithLanguage(language.langCode)
+        utterance.volume = 1F
     }
 
     actual fun speak(msg: String) {
+        synthesizer.speakUtterance(utterance)
     }
 
     actual fun stop() {
